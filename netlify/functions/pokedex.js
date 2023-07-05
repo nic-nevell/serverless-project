faf66fa80f6b5cc5f169a2919b72c0e7f73554af
nic <59603433+nic-nevell@users.noreply.github.com>
2023-07-05 13:37:15 +0100

    testing pull

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,12 +1,17 @@
-exports.handler = async function () {
-  const POKE_API = 'https://pokeapi.co/api/v2/pokemon/ditto'
+exports.handler = async function (event, context) {
+  const eventBody = JSON.parse(event.body);
+  console.log(eventBody);
+  const POKE_API = 'https://pokeapi.co/api/v2/pokemon/' + eventBody.name;
 
-  const response = await fetch(POKE_API)
-  const data = await response.json()
+  const response = await fetch(POKE_API);
+  const data = await response.json();
 
   return {
     statusCode: 200,
-    body: JSON.stringify(data)
-  }
-
-}
+    body: JSON.stringify({
+      // data
+      pokemon: data.name,
+      powers: data.abilities
+    })
+  };
+};
